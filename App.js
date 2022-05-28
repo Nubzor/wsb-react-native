a8cdecf039ab9602bee82465a07172d0a00dbc07
Mateusz Marzęcki <mmarzecki@Mateuszs-MacBook-Air.local>
2022-05-28 10:53:55 +0200

    Dodanie loaderkow i przycisku

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, Button } from 'react-native';
 
 function App() {
-  const animating = false;
+  const animating = true;
 
   return (
     <View style={styles.container}>
-      <ActivityIndicator animating={animating} hidesWhenStopped={false} />
+      <ActivityIndicator animating={animating} />
+      <ActivityIndicator animating={animating} size="large" />
+      <ActivityIndicator animating={animating} size={124} />
+      <ActivityIndicator animating={animating} style={[{}, {
+        transform: [{ scaleY: 5 }, { scaleX: 5 }]
+      }]} color="#ff0000"/>
       <Text>Open up App.js to start working on dsadasdyour app! adasdasdasdasd</Text>
+      <Button title="To jest przycisk" onPress={() => console.log('wcisniety!')} disabled={true} />
       <StatusBar style="auto" />
     </View>
   );
