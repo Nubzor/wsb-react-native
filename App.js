6384e463af2e540cff772528b667f3ced8b6d5af
Mateusz Marzęcki <mmarzecki@Mateuszs-MacBook-Air.local>
2022-05-28 11:38:41 +0200

    Zamiana na komponent klasowy

--- App.js
+++ App.js
@@ -2,23 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ActivityIndicator, Button, Image } from 'react-native';
 
-function App() {
-  const animating = true;
+class App extends React.Component {
+  render() {
+    const animating = true;
 
-  return (
-    <View style={styles.container}>
-      <ActivityIndicator animating={animating} />
-      <ActivityIndicator animating={animating} size="large" />
-      <ActivityIndicator animating={animating} size={124} />
-      <Text>Open up App.js to start working on dsadasdyour app! adasdasdasdasd</Text>
-      <Button title="To jest przycisk" onPress={() => console.log('wcisniety!')} disabled={true} />
-      <StatusBar style="auto" />
-      <Image 
-        fadeDuration={3000}
-        source={require('./default.png')} 
-        />
-    </View>
-  );
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator animating={animating} />
+        <ActivityIndicator animating={animating} size="large" />
+        <ActivityIndicator animating={animating} size={124} />
+        <Text>Open up App.js to start working on dsadasdyour app! adasdasdasdasd</Text>
+        <Button title="To jest przycisk" onPress={() => console.log('wcisniety!')} disabled={true} />
+        <StatusBar style="auto" />
+        <Image 
+          fadeDuration={3000}
+          source={require('./default.png')} 
+          />
+      </View>
+    );
+  }
 }
 
 export default App;
