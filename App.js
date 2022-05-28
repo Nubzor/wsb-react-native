8e3d2c3584a87e447fc4363e558ef0468fc613da
Mateusz Marzęcki <mmarzecki@Mateuszs-MacBook-Air.local>
2022-05-28 14:09:30 +0200

    Galeria zdjec

--- App.js
+++ App.js
@@ -1,47 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Button, Image, Modal, TouchableHighlight } from 'react-native';
+import { StyleSheet, View, Button, Image, Modal, Text, TouchableHighlight } from 'react-native';
+
+const images = [
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+    description: 'to jest jakis opis mojeg wspaniałego zdjecia',
+  },
+  {
+    url: 'https://www.tailorbrands.com/wp-content/uploads/2020/06/mcdonalds-logo-a-1.png',
+    description: 'to jest inny opis mojeg drugiego wspaniałego zdjecia',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297__340.png',
+    description: 'to jest inny opis trzeciego wspaniałego zdjecia',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+  {
+    url: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+  },
+];
 
 class App extends React.Component {
   state = {
     isModalVisible: false,
+    modalImageUrl: '',
+    modalDescription: '',
   }
 
-  onModalPressHandler = () => {
+  onModalPressHandler = (visible, image) => {
     this.setState({
-      isModalVisible: true,
+      isModalVisible: visible,
+      modalImageUrl: image && image.url,
+      modalDescription: image && image.description
     });
-  }
-
-  onModalClosePressHandler = () => {
-    this.setState({
-      isModalVisible: false,
-    })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        {/* <ActivityIndicator animating={this.state.isAnimating} size="large" /> */}
         <StatusBar style="auto" />
 
-        <TouchableHighlight onPress={this.onModalPressHandler}>
-          <Image 
-            fadeDuration={3000}
-            source={require('./default.png')} 
+        {images.map(image => {
+          return (<TouchableHighlight key={image.url} onPress={() => this.onModalPressHandler(true, image)} style={styles.imageContainer}>
+            <Image
+              resizeMode="contain"
+              source={{
+                uri: image.url,
+                width: 100,
+                height: 100,
+              }} 
             />
-        </TouchableHighlight>
+          </TouchableHighlight>)
+        })}
         
         <Modal visible={this.state.isModalVisible} transparent={true}>
           <View style={styles.modalContainer}>
             <Image 
+              fadeDuration={3000}
+              resizeMode="contain"
               source={{
-                uri: 'https://v.wpimg.pl/MDc0OC5wYiUCUjlwGgxvMEEKbSpcVWFmFhJ1YRpFfHwbAXxzGgQqMxMeODNWEyNqE1xgIgdHdCZWBXl1DUYvJloELnZTRSl9U1N3IQVDfHRUBHdtRRkqZh4',
+                uri: this.state.modalImageUrl,
                 width: 300,
-                height: 200,
+                height: 300,
               }} 
             />
-            <Button title="Zamknij" onPress={this.onModalClosePressHandler}/>
+            <Text>{this.state.modalDescription}</Text>
+            <Button title="Zamknij" onPress={() => this.onModalPressHandler(false)}/>
           </View>
         </Modal>
       </View>
@@ -57,14 +114,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#bbb',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingTop: 30,
   },
-  button: {
-    border: '1px solid',
-    borderColor: '#ccddee',
-  },
-  image: {
-    width: '100px',
-    height: '100px',
+  imageContainer: {
+    backgroundColor: '#fff',
+    padding: 5,
+    margin: 5,
   },
   modalContainer: {
     backgroundColor: '#fff',
